01ab5b2692dc96ea715e1ced75779a92325b2a6a
wongyuh <wongyuh36@gmail.com>
2021-10-21 22:27:07 -0700

    Added 3rd party & Object Pool scripts

---
+++ Assets/Scripts/GameObjectPool.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGameObjectPool
+{
+    void CreatePool();
+    void ClearPool();
+    GameObject GetObject();
+}
+
+[AddComponentMenu("Core/GameObjectPool")]
+public class GameObjectPool : MonoBehaviour, IGameObjectPool
+{
+    public GameObject prefab;
+    public int poolSize;
+
+    public List<PooledGameObject> AvaliableObjects = new List<PooledGameObject>();
+
+    private void Awake()
+    {
+        if (prefab != null)
+            CreatePool();
+    }
+
+    public void CreatePool()
+    {
+        while (AvaliableObjects.Count < poolSize)
+            AddObject(CreateObject());
+    }
+
+    public virtual void ClearPool()
+    {
+        for (int i = 0; i < AvaliableObjects.Count; ++i)
+            DestroyImmediate(AvaliableObjects[i].gameObject);
+        AvaliableObjects.Clear();
+    }
+
+    public GameObject GetObject()
+    {
+        if (AvaliableObjects.Count == 0)
+            AddObject(CreateObject());
+
+        var po = AvaliableObjects[AvaliableObjects.Count - 1];
+        po.Activate();
+
+        AvaliableObjects.RemoveAt(AvaliableObjects.Count - 1);
+        
+        return po.gameObject;
+    }
+
+    public void AddObject(PooledGameObject po)
+    {
+        if (AvaliableObjects.Contains(po))
+        {
+            Debug.LogError("AddObject Twise Error");
+        }
+        else
+        {
+            po.gameObject.SetActive(false);
+            po.transform.SetParent(transform, false);
+            AvaliableObjects.Add(po);
+        }
+    }
+
+    protected PooledGameObject CreateObject()
+    {
+        var go = Instantiate(prefab) as GameObject;
+        go.name = prefab.name;
+        var po = go.GetComponent<PooledGameObject>();
+        po.Pool = this;
+        return po;
+    }
+}
---
+++ Assets/Scripts/PooledGameObject.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public interface IPooledGameObject
+{
+    void ReturnToPool();
+    void Activate();
+}
+
+[AddComponentMenu("GameMaker/Core/Pooled GameObject")]
+public class PooledGameObject : MonoBehaviour, IPooledGameObject
+{
+    public GameObjectPool Pool;
+
+    public System.Action<PooledGameObject> onReturnToPoolCallback = null;
+
+    [BoxGroup("AutoReturnToPool")]
+    public bool enabledAutoReturnToPool = false;
+    [BoxGroup("AutoReturnToPool")]
+    public float time;
+
+    public virtual void ReturnToPool()
+    {
+        if (onReturnToPoolCallback != null)
+        {
+            onReturnToPoolCallback(this);
+            onReturnToPoolCallback = null;
+        }
+
+        if (Pool)
+        {
+            Pool.AddObject(this);
+        }
+        else
+        {
+            Debug.LogWarning("[ObjectPool] PooledObject has not pool.");
+            
+            Destroy(gameObject);
+        }
+    }
+
+    public virtual void Activate()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private Coroutine coroutine = null;
+    private IEnumerator IWaitForSeconds()
+    {
+        yield return new WaitForSeconds(time);
+
+        ReturnToPool();	
+    }
+
+    private void OnEnable()
+    {
+        if (enabledAutoReturnToPool)
+        {
+            if (coroutine != null)
+                StopCoroutine(coroutine);
+            coroutine = StartCoroutine(IWaitForSeconds());
+        }
+    }
+}
